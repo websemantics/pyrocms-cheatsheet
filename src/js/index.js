9ca3d3024e00b7648e1a8eeb21b73d1228459fe0
Adnan <adnan@websemantics.ca>
2016-08-20 23:28:45 -0400

    See Changlelog

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
 /*
-| PyroCMS 3 Cheat Sheet - A quick reference cheat sheets.
-|
-|  ____  __ __  ____   ___     __  ___ ___  _____
-| |    \|  |  ||    \ /   \   /  ]|   |   |/ ___/
-| |  o  )  |  ||  D  )     | /  / | _   _ (   \_
-| |   _/|  ~  ||    /|  O  |/  /  |  \_/  |\__  |
-| |  |  |___, ||    \|     /   \_ |   |   |/  \ |
-| |  |  |     ||  .  \     \     ||   |   |\    |
-| |__|  |____/ |__|\_|\___/ \____||___|___| \___|
-| Cheat Sheets
-|
-| This project was released under MIT license.
+| cheatsheet : Generic project template
 |
 | @link      http://websemantics.ca
 | @author    Web Semantics, Inc. Dev Team <team@websemantics.ca>
@@ -20,176 +9,166 @@
 window.jQuery = window.$ = require('jquery')
 window.Masonry = require('masonry-plus')
 window.Handlebars = require('handlebars')
+window.Prism = require('prismjs')
+window.Gitters = require('gitters')
+window.parse = require('markdown-html-ast').parse
+window._ = require("underscore.string")
 
 require('jquery-highlight')
-require('google-code-prettify/bin/prettify.min')
+require('prismjs/components/prism-php')
+require('prismjs/components/prism-bash')
 require('semantic-ui/dist/components/sidebar')
 require('semantic-ui/dist/components/sticky')
 require('semantic-ui/dist/components/checkbox')
 require('semantic-ui/dist/components/tab')
 require('semantic-ui/dist/components/visibility')
 
-$(function () {
-  var masonry
-  var lights = true
-  var comments = true
-  var url = 'cheats/'
-  var config = 'config.json'
-  var colors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey']
-  var templates = ['<div class="ui clearing brick segment" data-filter="{{id}}">\
-                          <div class="header"><i class="right floated {{icon}} icon"></i>\
-                          {{title}}</div>\
-                          <div class="ui divider"></div>\
-                          <pre class="prettyprint lang-{{lang}}">{{{content}}}</pre>\
-                        </div>', '<div data-filter="{{id}}" class="ui {{color}} inverted tiny basic button">{{title}}</div>',
-    '<a class="ui labeled mini button github-{{repository}}-stars">\
-                          <div class="ui  mini button"> <i class="star icon"></i> Stars </div>\
-                          <div class="ui basic  left pointing label"> <i class="spinner loading icon"></i> </div>\
-                      </a>\
-                      <a class="ui  mini button github-{{repository}}-github"> <i class="github icon"></i> Github </a>'
-  ]
+Gitters.defaults({
+    clearOnStart: false,
+})
 
-  var template = Handlebars.compile(templates[0])
-  var button = Handlebars.compile(templates[1])
-  var bragit = Handlebars.compile(templates[2])
+$(function() {
+    var masonry
+    var lights = true
+    var comments = true
+    var icon = "file text outline"
+    var title = "PyroCMS 3 Cheatsheets"
+    var repo = 'websemantics/pyrocms-cheatsheet'
+    var colors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey']
+    var segment = Handlebars.compile($("#segment-template").html())
+    var button = Handlebars.compile($("#button-template").html())
+    var bragit = Handlebars.compile($("#bragit-template").html())
 
-  /**
-   * Called when the config file is loaded.
-   *
-   * @param {cheats} json object contains list of cheats files and other configs
-   * @return {void}
-   */
-  function onConfigLoad (cheats) {
-    lights = cheats.lights
-    comments = cheats.comments
+    /**
+     * Called when all cheat files are fully loaded
+     *
+     * @return {void}
+     */
+    function onCheatsheetsLoaded() {
 
-    /* Make repository compatible with Bragit, */
-    cheats.repository = cheats.repository.replace('/', '-')
+        $('.toc .ui.sticky').sticky({
+            ontext: $('.pusher > .full.height')
+        })
 
-    $('.cheats-title').text(cheats.title)
+        $('#slider').sidebar('attach events', '.launch.button, .view-ui, .launch.item')
+            .sidebar('setting', {
+                dimPage: false
+            })
 
-    /* load all cheats.files */
-    $.when.apply(this, cheats.files.map(function (cheat) {
-      return $.get(url + cheat.name)
-    })).then(function () {
-      var args = cheats.files.length > 1 ? arguments : [arguments]
-      for (var i in cheats.files) {
-        var cheat = cheats.files[i]
-        cheat.content = args[i][0]
-        cheat.id = cheat.name.slice(0, -4)
-        cheat.color = colors[i % 13]
+        masonry = new Masonry('.masonry', {
+            'itemSelector': '.brick',
+            columnWidth: '.brick'
+        })
 
-        $('#content').append(template(cheat))
-        $('.filter').append(button(cheat))
-      }
-      /* $('.item.bragit').append(bragit(cheats)); TODO */
-      onCheatsLoad()
+        /* SETUP EVENTS */
+
+        /* Dismiss search highlights */
+        $(document).click(function(e) {
+            $('body').unhighlight()
+        })
+
+        /* Highlight search terms when search button is clicked */
+        $('.search-btn').click(function(event) {
+            event.stopPropagation()
+            $('body').highlight($('#sidebar-search').val())
+        })
+
+        /* Toggle buttons for light, comments and shuffles */
+        $('.lights-toggle').checkbox().click(toggleLights)
+        $('.comments-toggle').checkbox().click(toggleComments)
+        $('.shuffle-toggle').checkbox().click(function() {
+            masonry.layout({
+                shuffle: true
+            })
+        })
+
+        /* Search when 'enter' key is pressed */
+        $('.search-input').keypress(function(event) {
+            event.stopPropagation()
+            if (event.which == 13) {
+                $('body .full.height').highlight($(this).val())
+                return false
+            }
+        })
+
+        /* Filter on button clicks */
+        $('.filter .button').on('click', function() {
+            var filterValue = $(this).data('filter')
+            $('#content')[filterValue === '*' ? 'removeClass' : 'addClass']('force-one')
+            masonry.layout({
+                filter: filterValue
+            })
+        })
+
+        toggleComments()
+        toggleLights()
+    }
+
+    /**
+     * Turn lights on / off
+     *
+     * @return {void}
+     */
+
+    function toggleLights() {
+        $('#header-search')[lights ? 'removeClass' : 'addClass']('inverted')
+        $('.main.menu')[lights ? 'removeClass' : 'addClass']('inverted')
+        $('.full.height .ui.segment')[lights ? 'removeClass' : 'addClass']('inverted')
+        $('.full.height')[lights ? 'removeClass' : 'addClass']('inverted')
+        $('.ui.container')[lights ? 'removeClass' : 'addClass']('inverted')
+        $('.lights-toggle label').html(lights ? '<i class="ui icon moon"></i>' : '<i class="ui icon sun"></i>')
+        $('.ui.inverted.menu .svg.logo')[lights ? 'removeClass' : 'addClass']('inverted')
+        lights = !lights
+    }
+
+    /**[comments ? 'show' : 'hide']
+     * Turn comments on / off
+     *
+     * @return {void}
+     */
+
+    function toggleComments() {
+        $('.comments-toggle label').html(comments ? '<i class="ui icon hide"></i>' : '<i class="ui icon unhide"></i>')
+        $('span.token.comment')[comments ? 'show' : 'hide']()
+        // Prism.highlightAll()
+        masonry.layout()
+        comments = !comments
+    }
+
+    /* change the title and read the cheatsheets from the project README.md */
+
+    $('.cheats-title').text(title)
+
+    Gitters.fetch(repo, 'README.md', function(file) {
+
+        var elements = parse(file.content).children,
+            cheatsheets
+
+        elements.forEach(function(item, index, elements) {
+
+            /* mark the chaetsheets section, and parse all the relevent headings / code elements */
+            if (cheatsheets = cheatsheets ? item.element != 'h2' : item.element === 'h2' &&
+                item.value === 'Cheatsheets') {
+                  if(item.element === 'h3'){
+
+                    var code = elements[index + 1]
+
+                    var data = {
+                      id: _.underscored(item.value),
+                      title: item.value,
+                      icon : icon,
+                      lang: code.language,
+                      color: colors[index % 13],
+                      content: Prism.highlight(code.value, Prism.languages[code.language])
+                    }
+
+                    $('#content').append(segment(data))
+                    $('.filter').append(button(data))
+                  }
+            }
+        })
+
+        /* $('.item.bragit').append(bragit(cheats)); TODO */
+        setTimeout(onCheatsheetsLoaded,300);
     })
-  }
-
-  /**
-   * Called when all cheat files are fully loaded
-   *
-   * @return {void}
-   */
-  function onCheatsLoad () {
-    $('.toc .ui.sticky').sticky({
-      ontext: $('.pusher > .full.height')
-    })
-
-    $('#slider').sidebar('attach events', '.launch.button, .view-ui, .launch.item')
-      .sidebar('setting', {
-        dimPage: false
-      })
-
-      masonry = new Masonry('.masonry', {
-        'itemSelector': '.brick',
-        columnWidth: '.brick'
-      })
-
-    prettyPrint()
-    setupEvents()
-    toggleComments()
-    toggleLights()
-  }
-
-  /**
-   * Setup events
-   *
-   * @return {void}
-   */
-
-  function setupEvents () {
-    /* Dismiss search highlights */
-    $(document).click(function (e) {
-      $('body').unhighlight()
-    })
-
-    /* Highlight search terms when search button is clicked */
-    $('.search-btn').click(function (event) {
-      event.stopPropagation()
-      $('body').highlight($('#sidebar-search').val())
-    })
-
-    /* Toggle buttons for light, comments and shuffles */
-    $('.lights-toggle').checkbox().click(toggleLights)
-    $('.comments-toggle').checkbox().click(toggleComments)
-    $('.shuffle-toggle').checkbox().click(function () {
-      masonry.layout({shuffle:true})
-    })
-
-    /* Search when 'enter' key is pressed */
-    $('.search-input').keypress(function (event) {
-      event.stopPropagation()
-      if (event.which == 13) {
-        $('body .full.height').highlight($(this).val())
-        return false
-      }
-    })
-
-    /* Filter on button clicks */
-    $('.filter .button').on('click', function () {
-      var filterValue = $(this).data('filter')
-      $('#content')[filterValue === '*' ? 'removeClass' : 'addClass']('force-one')
-      masonry.layout({filter:filterValue})
-      prettyPrint()
-    })
-  }
-
-  /**
-   * Turn lights on / off
-   *
-   * @return {void}
-   */
-
-  function toggleLights () {
-    $('#header-search')[lights ? 'removeClass' : 'addClass']('inverted')
-    $('.main.menu')[lights ? 'removeClass' : 'addClass']('inverted')
-    $('.full.height .ui.segment')[lights ? 'removeClass' : 'addClass']('inverted')
-    $('.full.height')[lights ? 'removeClass' : 'addClass']('inverted')
-    $('.ui.container')[lights ? 'removeClass' : 'addClass']('inverted')
-    $('.lights-toggle label').html(lights ? '<i class="ui icon moon"></i>' : '<i class="ui icon sun"></i>')
-    $('.ui.inverted.menu .svg.logo')[lights ? 'removeClass' : 'addClass']('inverted')
-    lights = !lights
-  }
-
-  /**[comments ? 'show' : 'hide']
-   * Turn comments on / off
-   *
-   * @return {void}
-   */
-
-  function toggleComments () {
-    $('.comments-toggle label').html(comments ? '<i class="ui icon hide"></i>' : '<i class="ui icon unhide"></i>')
-    $('span.com')[comments ? 'show' : 'hide']()
-    masonry.layout()
-    comments = !comments
-  }
-
-  /* Load config file*/
-  $.ajax({
-    url: config,
-    dataType: 'json',
-    success: onConfigLoad
-  })
 })
